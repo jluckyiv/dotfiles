83c801277f8917e268607d34141d6305e6b1b102
Jack Lucky <jack.lucky.iv@gmail.com>
2019-02-11 13:43:02 -0800

    Move asdf to bottom

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,9 @@
 set -U EDITOR nvim
 
-source ~/.asdf/asdf.fish
 
 alias git "hub"
 alias ts "tmux new -s"
 alias vim "nvim"
 
 set -gx PATH $PATH /Users/jluckyiv/.local/bin
+source ~/.asdf/asdf.fish
