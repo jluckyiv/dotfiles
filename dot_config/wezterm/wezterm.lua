36dceb34f8f9509241d1dc1eed4cf7df3c9863b3
Jackson Lucky <jack@jacksonlucky.net>
2023-06-04 17:56:26 -0400

    Fix wezterm colorscheme.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -13,15 +13,16 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-local function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Latte"
-	end
-end
-
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+-- local function scheme_for_appearance(appearance)
+-- 	if appearance:find("Dark") then
+-- 		return "Catppuccin Mocha"
+-- 	else
+-- 		return "Catppuccin Latte"
+-- 	end
+-- end
+--
+-- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 17.0
 config.window_background_opacity = 0.85
