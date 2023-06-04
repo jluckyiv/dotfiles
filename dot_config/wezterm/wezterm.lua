1c8c0ff3e0d88123d1d73cfaf593a427f7320d5e
Jackson Lucky <jack@jacksonlucky.net>
2023-06-04 17:40:38 -0400

    Add wezterm.

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,30 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 17.0
+config.window_background_opacity = 0.85
+
+-- and finally, return the configuration to wezterm
+return config
