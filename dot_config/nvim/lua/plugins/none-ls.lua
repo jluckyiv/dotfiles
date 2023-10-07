d7d851e52b8cb702735be0adf1944f1007a2fa78
Jackson Lucky <jack@jacksonlucky.net>
2023-10-07 12:56:34 -0700

    Update some LazyVim settings.

---
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,19 @@
+return {
+  "nvimtools/none-ls.nvim",
+  event = "LazyFile",
+  dependencies = { "mason.nvim" },
+  opts = function()
+    local nls = require("null-ls")
+    return {
+      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      sources = {
+        nls.builtins.formatting.fish_indent,
+        nls.builtins.diagnostics.fish,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.formatting.markdownlint,
+        -- nls.builtins.diagnostics.flake8,
+      },
+    }
+  end,
+}
