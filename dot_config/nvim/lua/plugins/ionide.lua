fdbc7267ddb71f825e24de1b10264b4626ef81ef
Jackson Lucky <jack@jacksonlucky.net>
2024-01-30 04:53:30 -0800

    Fix ionide config.

--- dot_config/nvim/lua/plugins/ionide.lua
+++ dot_config/nvim/lua/plugins/ionide.lua
@@ -1,11 +1,4 @@
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end,
-  },
   { "ionide/Ionide-vim" },
 }
 return plugins
