27d79e484afaabaed646fab755b8ab1963398036
Jackson Lucky <jack@jacksonlucky.net>
2024-04-03 18:24:34 -0700

    Make everything Tokyo Night.

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,9 +1,12 @@
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		-- return "Catppuccin Mocha"
-		return "Tokyo Night"
+		return "Catppuccin Mocha"
+		-- return "Tokyo Night"
 	else
+		-- return "Catppuccin Latte"
+		-- return "Catppuccin Mocha"
 		-- return "Tokyo Night Day"
+		return "Tokyo Night"
 	end
 end
 -- Pull in the wezterm API
@@ -15,7 +18,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme = "Tokyo Night"
 config.window_background_opacity = 0.9
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
