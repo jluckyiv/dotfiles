13dd5cd48acc3f7ca3906578a88ddccfea19173f
Jackson Lucky <jack@jacksonlucky.net>
2024-11-26 12:16:33 -0800

    Update scheduling order scripts to prepend dates.

--- dot_config/bin/scomd.sh
+++ dot_config/bin/scomd.sh
@@ -5,7 +5,8 @@ regex='.*([0-9]{10})$'
 
 if [[ $folder_name =~ $regex ]]; then
 	folder_name="${BASH_REMATCH[1]}"
-	touch "scheduling-order-$folder_name.md"
+	current_date=$(date +%Y-%m-%d)
+	touch "${current_date}-scheduling-order-$folder_name.md"
 else
 	echo "Folder name does not end in 10 digits"
 fi
